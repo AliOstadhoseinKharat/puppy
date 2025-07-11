1ddce806e23d8e577d0858778b4e02b5e7e890d3
Ali <a.ostadhosein@iccima.ir>
2025-07-12 00:40:40 +0330

    version 1.0.0 commited !

--- puppy.go
+++ puppy.go
@@ -1,6 +1,8 @@
 package puppy
 
 import (
+	"fmt"
+
 	"github.com/AliOstadhoseinKharat/dog"
 )
 
@@ -18,4 +20,8 @@ func BigBark() string {
 
 func BigBarks() string {
 	return dog.WhenGrowUp(Barks())
+}
+
+func version1() {
+	fmt.Println("I'm from version 1.0.0")
 }
